aeec340db6448af8abf9a0068e0368d5c495ef6e
Vidal <vidalesnar@gmail.com>
2020-03-23 03:34:14 +0100

    Añadidos todos los sprites, animaciones hechas e inicio de movimiento

---
+++ VEUS/Assets/Scripts/MovementController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float movementSpeed = 3.0f;
+    Vector2 movement = new Vector2();
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement.Normalize();
+        rb2d.velocity = movement * movementSpeed;
+    }
+}
