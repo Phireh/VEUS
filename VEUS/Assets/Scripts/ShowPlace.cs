ceddac6e6e51b637bbd58b9a1446c98538a405c8
Vidal <vidalesnar@gmail.com>
2020-04-20 16:26:24 +0200

    ArrowPointer and displaying place name

---
+++ VEUS/Assets/Scripts/ShowPlace.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPlace : MonoBehaviour
+{
+    public BoxCollider2D place;
+    public Animator anim;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            anim.SetTrigger("activado");
+        }
+        
+    }
+}
---
+++ VEUS/Assets/Scripts/TargetIndicator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetIndicator : MonoBehaviour
+{
+    public Transform target;
+    public float HideDistance;
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+
+        if(dir.magnitude < HideDistance)
+        {
+            foreach(Transform child in transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
+
